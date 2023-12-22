b15e3540f72ae3aa07074b7523b4cb6136d875ad
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-22 13:57:10 +0600

    MongoDB is setup

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.post('/new-todo', (req, res) => {
+    const data = req.body;
+    console.log(data);
+});
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lmbjtpv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true } });
+
+async function run() {
+    try {
+
+        await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => res.send('Task Manager Server is Running'));
 
